91211209a7d5a7c990fd44fdfadaaa0e3a4dc986
StephenOosthuizen <StephenDiavolo@gmail.com>
2024-03-04 12:18:31 +0200

    Add project files.

---
+++ MessageEncoding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DelegateEventsTest.MessageEncoding;
+
+//file-scoped namespace
+namespace DelegateEventsTest;
+internal class MessageEncoding
+{
+    //the actual event that gets fired - although, this isnt called directly, this is just a definition of the event
+    //  the object of this type is what gets called and has stuff listen for it
+    public delegate void MessageEncodedEventHandler(object obj, EventArgs e);
+    public event MessageEncodedEventHandler? MessageEncodedEvent;
+    public string messageEncodingObjectDataString = "";
+
+    //do some stuff, then call the method that will raise the event
+    public void messageEncode()
+    {
+        Console.WriteLine("Encoding the message for you..");
+        Thread.Sleep(100);
+        onMessageEncoded();
+    }
+    protected virtual void onMessageEncoded()
+    {
+        if(MessageEncodedEvent != null) 
+        {
+            //just loading up our object's string with some data (This is part of a reusability test/concept I was messing around with)
+            messageEncodingObjectDataString = "hello world!";
+
+            //raise the event (Basically just updating the event object, by passing in some data)
+            MessageEncodedEvent(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+//file-scoped namespace
+namespace DelegateEventsTest;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new instances of the classes
+            MessageEncoding newEncoding = new MessageEncoding();
+            SendEmail sendEmail = new SendEmail();
+            SendWhatsapp sw = new SendWhatsapp();
+            NotifySent nfs = new NotifySent();
+            SendSteamMessage steamMessage = new SendSteamMessage();
+
+            //load up the subscriber methods to the publisher method (subscriber method's input equals the Event's)
+            newEncoding.MessageEncodedEvent += sendEmail.subscriber;
+            newEncoding.MessageEncodedEvent += sw.subscriber;
+            newEncoding.MessageEncodedEvent += steamMessage.subscriber;
+
+            //making it more extensible and less-coupled by having a "message" function that all classes share
+            //  this is nice, because we dont explicitly call a method anywhere, we just fire the event
+            sw.messageSent += nfs.subscriber;
+            sendEmail.messageSent += nfs.subscriber;
+            steamMessage.messageSent += nfs.subscriber;
+
+            //now we need to actually exec the method - or else it won't work
+            newEncoding.messageEncode();
+
+            //unsubscribe from the events - this helps to ensure there are no memory leaks or piles of objects in memory, etc.
+            newEncoding.MessageEncodedEvent -= sendEmail.subscriber;
+            newEncoding.MessageEncodedEvent -= sw.subscriber;
+            newEncoding.MessageEncodedEvent -= steamMessage.subscriber;
+        }
+    }
+
+    class SendEmail
+    {
+        public delegate void messageSentEventHandler(string s);
+        public event messageSentEventHandler? messageSent;
+
+        public void onMessageSent()
+        {
+            if (messageSent != null)
+            {
+                messageSent("Email Sent");
+            }
+        }
+
+        public void subscriber(object obj, EventArgs e)
+        {
+            Console.WriteLine("Sending email..");
+            Thread.Sleep(100);
+            onMessageSent();
+        }
+    }
+
+    class SendSteamMessage
+    {
+        public delegate void messageSentEventHandler(string s);
+        public event messageSentEventHandler? messageSent;
+
+        public void subscriber(object obj, EventArgs e)
+        {
+            Console.WriteLine("Sending message to Steam account");
+
+            //clever way of getting data from the source object
+            Console.WriteLine("Stephen test: " + ((MessageEncoding)obj).messageEncodingObjectDataString);
+            Thread.Sleep(100);
+            onMessageSent();
+        }
+        public void onMessageSent()
+        {
+            if (messageSent != null)
+            {
+                messageSent("Message Sent");
+            }
+        }
+    }
+    class SendWhatsapp
+    {
+        public delegate void messageSentEventHandler(string s);
+        public event messageSentEventHandler? messageSent;
+
+        public void onMessageSent()
+        {
+            if (messageSent != null)
+            {
+                messageSent("Whatsapp Sent");
+            }
+        }
+        public void subscriber(object obj, EventArgs e)
+        {
+            Console.WriteLine("Sending WhatsApp message..");
+            Thread.Sleep(100);
+            onMessageSent();
+        }
+    }
+
+    class NotifySent
+    {
+        public void subscriber(string s)
+        {
+            Thread.Sleep(100);
+            Console.WriteLine(s);
+        }
+    }
